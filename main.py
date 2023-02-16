e6c089bedd4e6f8e53de5c61ec5ed99da3bd881d
mikl123 <bulesnmisha3@gmil.com>
2023-02-16 11:10:56 +0200

    Final Commit 2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+"""
+HTML map
+"""
 import math
 import argparse
 from geopy.geocoders import Nominatim
@@ -7,19 +10,19 @@ from geopy.extra.rate_limiter import RateLimiter
 from random import uniform
 
 
-def tass_points(list:list, index_to_tass):
+def tass_points(arr: list, index_to_tass_arr: list):
     """
     Randomly tass points
     """
-    move=0.008
-    for i in index_to_tass:
-        x_coef=uniform(-1,1)
-        y_coef=uniform(-1,1)
-        current_location=list[i][0]
-        new_location=[0,0]
-        new_location[0]=current_location[0]+x_coef*move
-        new_location[1]=current_location[1]+y_coef*move
-        list[i][0]=tuple(new_location)
+    move = 0.008
+    for i in index_to_tass_arr:
+        x_coef = uniform(-1,1)
+        y_coef = uniform(-1,1)
+        current_location = arr[i][0]
+        new_location = [0,0]
+        new_location[0] = current_location[0] + x_coef * move
+        new_location[1] = current_location[1] + y_coef * move
+        arr[i][0] = tuple(new_location)
 
 
 parser = argparse.ArgumentParser()
@@ -28,32 +31,31 @@ parser.add_argument('latitude')      # option that takes a value
 parser.add_argument('longitude')
 parser.add_argument('path_to_dataset')
 args = parser.parse_args()
-year_chose=args.year
-latitude=float(args.latitude)
-longitude=float(args.longitude)
-path_to_dataset=args.path_to_dataset
+year_chose = args.year
+latitude = float(args.latitude)
+longitude = float(args.longitude)
+path_to_dataset = args.path_to_dataset
 
 geolocator = Nominatim(user_agent="film_points.py")
 geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
 
-with open(path_to_dataset,"r",encoding="utf-8") as film_spots:
-    dictionary=[]
-    counter=0
+with open(path_to_dataset,"r",encoding = "utf-8") as film_spots:
+    dictionary = []
+    counter = 0
     # Gets location of certain year films spots
     while True:
-        counter+=1
-        line=film_spots.readline()
+        counter += 1
+        line = film_spots.readline()
         if line:
-            line_splited=line.split("\t")
-            year=line_splited[0].split(" ")[-1]
-            film_name=" ".join(line_splited[0].split(" ")[:-1])
-            place_name=line_splited[-1].strip()
+            line_splited = line.split("\t")
+            year = line_splited[0].split(" ")[-1]
+            film_name = " ".join(line_splited[0].split(" ")[:-1])
+            place_name = line_splited[-1].strip()
             if year_chose in year:
                 try:
                     location = geocode(place_name)
-                    print(counter)
                     coordinate = (location.latitude, location.longitude)
-                    distance_to_cooedinate=haversine(coordinate,(latitude,longitude))
+                    distance_to_cooedinate = haversine(coordinate,(latitude,longitude))
                     dictionary.append([coordinate,film_name,place_name,distance_to_cooedinate])
                 except AttributeError:
                     pass
@@ -62,14 +64,14 @@ with open(path_to_dataset,"r",encoding="utf-8") as film_spots:
         else:
             break
     # Sorts and get the nearest film spots
-    dictionary=sorted(dictionary,key=lambda film: film[-1])
-    map = folium.Map(location=[latitude, longitude], zoom_start=3)
+    dictionary=sorted(dictionary,key = lambda film: film[-1])
+    html_map = folium.Map(location = [latitude, longitude], zoom_start=3)
     fg_near_film_spot = folium.FeatureGroup(name="Near film spots")
-    nearest=dictionary[:10]
+    nearest = dictionary[:10]
     # Finds locations in the same place
-    used_location=[]
-    index_to_tass=[]
-    for index,value in enumerate(nearest):
+    used_location = []
+    index_to_tass = []
+    for index, value in enumerate(nearest):
         if value[0] not in used_location:
             used_location.append(value[0])
         else:
@@ -78,16 +80,17 @@ with open(path_to_dataset,"r",encoding="utf-8") as film_spots:
     tass_points(dictionary,index_to_tass)
     # Add markers
     for film in dictionary[:10]:
-        fg_near_film_spot.add_child(folium.Marker(location=[film[0][0], film[0][1]],
-                            popup=f"Назва фільму:\n {film[1]}\n Назва місця:\n {film[2]}",
-                            icon=folium.Icon()))
-    print(nearest[-1][-1]+50)
-    fg_near_film_area = folium.FeatureGroup(name="Near film Area")
-    fg_near_film_area.add_child(folium.Circle(location=[latitude, longitude],
-                                    popup=f'Area of close film spots of {year_chose} year',
-                                    fill_color='#040',
-                                    radius=(nearest[-1][-1]+100)*1000, weight=1, color="#004"))
-    map.add_child(fg_near_film_spot)
-    map.add_child(fg_near_film_area)
-    map.add_child(folium.LayerControl())
-    map.save('Map_Default.html')
+        fg_near_film_spot.add_child(folium.Marker(location = [film[0][0], film[0][1]],
+                            popup = f"Назва фільму:\n {film[1]}\n Назва місця:\n {film[2]}",
+                            icon = folium.Icon()))
+    fg_near_film_area = folium.FeatureGroup(name = "Near film Area")
+    fg_near_film_area.add_child(folium.Circle(location = [latitude, longitude],
+                                    popup = f'Area of close film spots of {year_chose} year',
+                                    fill_color = '#040',
+                                    radius = (nearest[-1][-1] + 100)*1000,
+                                    weight = 1,
+                                    color = "#004"))
+    html_map.add_child(fg_near_film_spot)
+    html_map.add_child(fg_near_film_area)
+    html_map.add_child(folium.LayerControl())
+    html_map.save('Map_Default.html')
