79e91915394bbd1d5e6400e6ce7eeb97ee0d2b3a
mikl123 <bulesnmisha3@gmil.com>
2023-02-16 10:55:26 +0200

    final commit

---
+++ get_data.py
@@ -0,0 +1,19 @@
+import random
+def get_lines(path_to_list:str, number_lines:int):
+    getted=""
+    all_file=[]
+    with open(path_to_list,"r",encoding="utf-8") as file_read:
+        while not file_read.readline().startswith("=="):
+            pass
+        while True:
+            readed_line=file_read.readline()
+            if readed_line:
+                all_file.append(readed_line)
+            else:
+                break
+        for _ in range(number_lines):
+            random_index=random.randint(100,1000000)
+            getted+=all_file[random_index]
+    with open("small_films.list","w",encoding="utf-8") as file_write:
+        file_write.write(getted)
+get_lines("D:\project_2\lab_1\map_repo\locations.list",15000)
